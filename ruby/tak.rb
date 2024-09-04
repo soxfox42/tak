0a41b0662cbeb5b4d3865d06c36a5e1407e60c3c
Ivan Volkov <ivanthegreat825@gmail.com>
2024-09-04 18:57:04 -0400

    cleaner args

--- ruby/tak.rb
+++ ruby/tak.rb
@@ -1,6 +1,6 @@
 def tak(x, y, z)
     if y < x
-        tak(
+        tak (
             tak(x - 1, y, z),
             tak(y - 1, z, x),
             tak(z - 1, x, y),
@@ -10,5 +10,5 @@ def tak(x, y, z)
     end
 end
 
-x, y, z = ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i
+x, y, z = ARGV.map(&:to_i)
 puts tak(x, y, z)
