fbe51cc621e94b3d33bcd1eb4d96640742f40023
Eloi Torrents <eloitor@disroot.org>
2024-10-24 13:55:42 +0200

    Add clojure

---
+++ clojure/tak.clj
@@ -0,0 +1,13 @@
+(defn tak [x y z]
+  (if (< y x)
+    (tak
+      (tak (dec x) y z)
+      (tak (dec y) z x)
+      (tak (dec z) x y))
+    z))
+
+(defn -main [& args]
+  (let [x (Integer/parseInt (nth args 0))
+        y (Integer/parseInt (nth args 1))
+        z (Integer/parseInt (nth args 2))]
+    (println (tak x y z))))
