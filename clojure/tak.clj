44ff2f035c376f18261cadfe1fd408e9a71a13f4
Finn Coffey <finn.coffey@gmail.com>
2024-10-28 14:28:08 +1100

    Make clojure work correctly when run as a script

--- clojure/tak.clj
+++ clojure/tak.clj
@@ -1,3 +1,5 @@
+(ns tak)
+
 (defn tak [x y z]
   (if (< y x)
     (tak
@@ -6,8 +8,8 @@
       (tak (dec z) x y))
     z))
 
-(defn -main [& args]
-  (let [x (Integer/parseInt (nth args 0))
-        y (Integer/parseInt (nth args 1))
-        z (Integer/parseInt (nth args 2))]
-    (println (tak x y z))))
+(def args *command-line-args*)
+(let [x (Integer/parseInt (nth args 0))
+      y (Integer/parseInt (nth args 1))
+      z (Integer/parseInt (nth args 2))]
+  (println (tak x y z)))
